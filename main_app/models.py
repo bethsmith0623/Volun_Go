f78d24044415154370a32e02c0d8bbd4fc21c817
Michael Martinez <rusalka2100@gmail.com>
2019-09-13 15:32:56 -0500

    Event Model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    location = models.CharField(max_length=100)
+    date = models.CharField(max_length=100)
+    orghost = models.CharField(max_length=100)
+    time = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
