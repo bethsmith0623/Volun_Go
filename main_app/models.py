6cae71d68eef10a0ccef65cf8fde25401bc6ab13
Michael Martinez <rusalka2100@gmail.com>
2019-09-16 09:47:42 -0500

    Models, urls

--- main_app/models.py
+++ main_app/models.py
@@ -1,12 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Event(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     location = models.CharField(max_length=100)
-    date = models.CharField(max_length=100)
-    orghost = models.CharField(max_length=100)
+    date = models.DateField()
     time = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
 
     def __str__(self):
-        return self.title
+        return self.title 
+
+
+class Organization(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phonenum = models.CharField(max_length=100)
+
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+# migrations for user model already made
+
+
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
+from .models import Event
 
-# Create your views here.
+
